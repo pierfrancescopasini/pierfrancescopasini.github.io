b40e2a1f4f3bf20b852d8d4914d7897101d38e43
Paolo Baiguera <paolo.baiguera.work@gmail.com>
2020-12-10 00:14:12 +0100

    Corrected for mobile

--- src/components/LinkTo.js
+++ src/components/LinkTo.js
@@ -1,26 +1,36 @@
 import React from 'react'
 
 
-class GridImage extends React.Component{
+class LinkTo extends React.Component{
     state = {
         mouseOver: false
     }
 
     render(){        
+        let widthOn = window.innerWidth/8;
+        let widthOff = window.innerWidth/10;
+        let mob = false
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            if (window.matchMedia("(orientation: portrait)").matches) {
+                widthOn = window.innerWidth/2;
+            }
+            mob = true;
+
+        }
         return (
             <div 
-            style={this.state.mouseOver?
-                  {width:window.innerWidth/8, height:'5%', color:'#282c34', borderRadius:'10px', backgroundColor:'#f5f5f5', left:0, right:0, transition:'0.5s'}
-                  : {width:window.innerWidth/10, height:'5%', color:'#282c34', borderRadius:'10px', backgroundColor:'#f5f5f5', left:0, right:0, transition:'0.5s'}
+            style={this.state.mouseOver || mob?
+                  {width:widthOn, height:'5%', color:'#282c34', borderRadius:'10px', backgroundColor:'#f5f5f5', left:0, right:0, transition:'0.5s'}
+                  : {width:widthOff, height:'5%', color:'#282c34', borderRadius:'10px', backgroundColor:'#f5f5f5', left:0, right:0, transition:'0.5s'}
             }
             onClick={() => {window.location=this.props.link}}
             onMouseEnter={() => {this.setState({mouseOver:true})}}
             onMouseLeave={() => {this.setState({mouseOver:false})}}
            >
-            Go To Event {this.state.mouseOver && <i className="arrow right"></i>}
+            Go To Event {(this.state.mouseOver || mob) && <i className="arrow right"></i>}
             </div>
         )
     }
 }
 
-export default GridImage
+export default LinkTo
